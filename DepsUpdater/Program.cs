1670dc805792c8805818b08ccc2d642d98dcf89f
Gérald Barré <meziantou@live.fr>
2021-04-21 22:46:47 -0400

    Add options

--- DepsUpdater/Program.cs
+++ DepsUpdater/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO.Enumeration;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
@@ -8,6 +11,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Meziantou.Framework;
 using Meziantou.Framework.DependencyScanning;
+using Meziantou.Framework.Globbing;
 using NuGet.Common;
 using NuGet.Protocol;
 using NuGet.Protocol.Core.Types;
@@ -19,24 +23,84 @@ namespace DepsUpdater
     {
         private static readonly HttpClient s_httpClient = new();
 
-        private static async Task Main(string[] args)
+        private static Task<int> Main(string[] args)
         {
+            var rootCommand = new RootCommand();
+            AddUpdateCommand(rootCommand);
+            return rootCommand.InvokeAsync(args);
+        }
+
+        private static void AddUpdateCommand(RootCommand rootCommand)
+        {
+            var replaceValueCommand = new Command("update")
+            {
+                new Option<string>(
+                    "--directory",
+                    arity: ArgumentArity.ZeroOrOne,
+                    description: "Root directory"),
+
+                new Option<string>(
+                    "--files",
+                    arity: ArgumentArity.ZeroOrMore,
+                    description: "Glob patterns to find files to update"),
+
+                new Option<string>(
+                    "--dependency-type",
+                    arity: ArgumentArity.ZeroOrMore,
+                    description: "NuGet, npm"),
+            };
+
+            replaceValueCommand.Description = "Update dependencies";
+            replaceValueCommand.Handler = CommandHandler.Create((string directory, string[] files, string[] dependencyType) => ReplaceValue(directory, files, dependencyType));
+
+            rootCommand.AddCommand(replaceValueCommand);
+        }
+
+        private static async Task<int> ReplaceValue(string directory, string[] filePatterns, string[] dependencyType)
+        {
+            GlobCollection? globs = null;
+            if (filePatterns.Length > 0)
+            {
+                globs = new(filePatterns.WhereNotNull().Select(pattern => Glob.Parse(pattern, GlobOptions.IgnoreCase)).ToArray());
+            }
+            else
+            {
+                globs = new(Glob.Parse("**/*", GlobOptions.None), Glob.Parse("!**/node_modules/**/*", GlobOptions.None));
+            }
+
+            var dependencyTypes = dependencyType.WhereNotNull().Select(Enum.Parse<DependencyType>).ToArray();
+
             var cancellationTokenSource = new CancellationTokenSource();
             Console.CancelKeyPress += (_, _) => cancellationTokenSource.Cancel();
-            var directory = Environment.CurrentDirectory;
-            if (args.Length > 0 && !string.IsNullOrEmpty(args[0]))
+
+            if (string.IsNullOrEmpty(directory))
             {
-                directory = args[0];
+                directory = Environment.CurrentDirectory;
             }
 
             var options = new ScannerOptions()
             {
                 RecurseSubdirectories = true,
+                ShouldScanFilePredicate = (ref FileSystemEntry entry) => globs.IsMatch(ref entry),
+                ShouldRecursePredicate = (ref FileSystemEntry entry) => globs.IsPartialMatch(ref entry),
+                Scanners = new DependencyScanner[]
+                {
+                    new Meziantou.Framework.DependencyScanning.Scanners.DotNetGlobalJsonDependencyScanner(),
+                    new Meziantou.Framework.DependencyScanning.Scanners.MsBuildReferencesDependencyScanner(),
+                    new Meziantou.Framework.DependencyScanning.Scanners.NuSpecDependencyScanner(),
+                    new Meziantou.Framework.DependencyScanning.Scanners.PackagesConfigDependencyScanner(),
+                    new Meziantou.Framework.DependencyScanning.Scanners.ProjectJsonDependencyScanner(),
+                    new Meziantou.Framework.DependencyScanning.Scanners.NpmPackageJsonDependencyScanner(),
+                },
             };
 
             var dependencies = await DependencyScanner.ScanDirectoryAsync(directory, options, cancellationTokenSource.Token).ToListAsync();
+            Console.WriteLine($"{dependencies.Count} dependencies found");
             foreach (var dependency in dependencies.Where(d => d.Location.IsUpdatable))
             {
+                if (dependencyTypes.Length > 0 && !dependencyTypes.Contains(dependency.Type))
+                    continue;
+
                 if (dependency.Type == DependencyType.NuGet)
                 {
                     await UpdateNuGetDependencyAsync(dependency, cancellationTokenSource.Token);
@@ -45,7 +109,12 @@ namespace DepsUpdater
                 {
                     await UpdateDotNetSDKDependencyAsync(dependency, cancellationTokenSource.Token);
                 }
+                else if (dependency.Type == DependencyType.Npm)
+                {
+                    await UpdateNpmDependencyAsync(dependency, cancellationTokenSource.Token);
+                }
             }
+            return 0;
         }
 
         private static async Task UpdateNuGetDependencyAsync(Dependency dependency, CancellationToken cancellationToken)
@@ -122,7 +191,7 @@ namespace DepsUpdater
             }
 
             var index = await s_httpClient.GetFromJsonAsync<DotNetReleaseIndex>("https://raw.githubusercontent.com/dotnet/core/main/release-notes/releases-index.json", cancellationToken);
-            var versions = index.Releases.Select(r => SemanticVersion.Parse(r.LastestSdk));
+            var versions = index!.Releases!.Select(r => SemanticVersion.Parse(r.LastestSdk));
 
             var latestVersion = versions.Where(ConsiderVersion).DefaultIfEmpty().Max();
             if (latestVersion == null)
@@ -131,19 +200,78 @@ namespace DepsUpdater
             await dependency.UpdateAsync(latestVersion.ToString(), cancellationToken);
         }
 
+        private static async Task UpdateNpmDependencyAsync(Dependency dependency, CancellationToken cancellationToken)
+        {
+            var version = dependency.Version;
+            if (version.Length > 0 && version[0] is '~' or '^')
+            {
+                version = version[1..];
+            }
+
+            if (!SemanticVersion.TryParse(version, out var currentVersion))
+                return;
+
+            bool ConsiderVersion(SemanticVersion version)
+            {
+                // Only consider newer version
+                if (version <= currentVersion)
+                    return false;
+
+                // Stable version are always considered
+                if (version.IsPrerelease)
+                {
+                    // 1.0.0-alpha -> 1.0.0-beta OK
+                    // 1.0.0-alpha -> 2.0.0      OK
+                    // 1.0.0       -> 2.0.0-beta KO
+                    // 1.0.0-alpha -> 2.0.0-beta KO
+                    if (!currentVersion.IsPrerelease)
+                        return false;
+
+                    if ((version.Major, version.Minor, version.Patch) == (currentVersion.Major, currentVersion.Minor, currentVersion.Patch))
+                        return true;
+                }
+
+                return true;
+            }
+
+            var index = await s_httpClient.GetFromJsonAsync<NpmPackageInfo>("https://registry.npmjs.org/" + dependency.Name, cancellationToken);
+            var versions = new[] { index!.DistTag!.Latest, index.DistTag.Next }.WhereNotNullOrWhiteSpace().Select(v => SemanticVersion.Parse(v));
+
+            var latestVersion = versions.Where(ConsiderVersion).DefaultIfEmpty().Max();
+            if (latestVersion == null)
+                return;
+
+            await dependency.UpdateAsync(latestVersion.ToString(), cancellationToken);
+        }
+
+        private sealed class NpmPackageInfo
+        {
+            [JsonPropertyName("dist-tags")]
+            public NpmPackageDistTag? DistTag { get; set; }
+        }
+
+        private sealed class NpmPackageDistTag
+        {
+            [JsonPropertyName("latest")]
+            public string? Latest { get; set; }
+
+            [JsonPropertyName("next")]
+            public string? Next { get; set; }
+        }
+
         private sealed class DotNetReleaseIndex
         {
             [JsonPropertyName("releases-index")]
-            public IReadOnlyCollection<DotNetReleaseEntry> Releases { get; set; }
+            public IReadOnlyCollection<DotNetReleaseEntry>? Releases { get; set; }
         }
 
         private sealed class DotNetReleaseEntry
         {
             [JsonPropertyName("channel-version")]
-            public string ChannelVersion { get; set; }
+            public string? ChannelVersion { get; set; }
 
             [JsonPropertyName("latest-sdk")]
-            public string LastestSdk { get; set; }
+            public string? LastestSdk { get; set; }
         }
     }
 }
